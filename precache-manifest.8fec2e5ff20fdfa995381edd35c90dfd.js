b923b254051aa105605c53d81dafda24ebeeee27
Omar Jbara <omar.jbara2@gmail.com>
2019-08-25 00:09:20 +0200

    Updates

---
+++ precache-manifest.8fec2e5ff20fdfa995381edd35c90dfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc83d469280d4bb8fe3c17550fb18e0",
+    "url": "/react-tabs/index.html"
+  },
+  {
+    "revision": "726cafa9fff27ca01061",
+    "url": "/react-tabs/static/css/main.73127b3b.chunk.css"
+  },
+  {
+    "revision": "688eb630244747d9ccab",
+    "url": "/react-tabs/static/js/2.7ab1a736.chunk.js"
+  },
+  {
+    "revision": "726cafa9fff27ca01061",
+    "url": "/react-tabs/static/js/main.28d0e4b2.chunk.js"
+  },
+  {
+    "revision": "0db9067de5f1aad57864",
+    "url": "/react-tabs/static/js/runtime~main.921716de.js"
+  }
+]);
